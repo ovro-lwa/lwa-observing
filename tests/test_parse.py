2901a8cd22dd4d3748fd06288754ff4d84a78ba4
Casey Law <caseyjlaw@gmail.com>
2023-08-05 01:05:20 +0000

    adding simple tests

---
+++ tests/test_parse.py
@@ -0,0 +1,17 @@
+import pytest
+import os.path
+from observing import parsesdf
+
+_install_dir = os.path.abspath(os.path.dirname(__file__))
+
+def test_read():
+    fn = os.path.join(_install_dir, 'test.sdf')
+    d = parsesdf.sdf_to_dict(fn)
+    assert isinstance(d, dict)
+
+
+def test_make():
+    fn = os.path.join(_install_dir, 'test.sdf')
+    d = parsesdf.sdf_to_dict(fn)
+    session, obs_list = parsesdf.make_obs_list(d)
+    df = parsesdf.power_beam_obs(obs_list, session)
