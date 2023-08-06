a4de1fa5ad92fe651adf98f970ff9e6a355db674
Casey Law <caseyjlaw@gmail.com>
2023-08-06 00:49:43 +0000

    working callback to set schedule object via etcd

--- scripts/executor.py
+++ scripts/executor.py
@@ -10,7 +10,7 @@ import sys
 from time import sleep
 from concurrent.futures import ProcessPoolExecutor, wait, as_completed
 
-from pandas import concat
+from pandas import concat, DataFrame
 from astropy.time import Time
 from observing.classes import ObsType
 from observing import parsesdf
@@ -38,7 +38,7 @@ def make_sched(sdf_fn):
     return sched
 
 
-def sched_update(sched=None):
+def sched_update(sched):
     """ Take a schedule or list of schedules, concatenate and sort them.
     Will either merge input list of scheds or get new sched from etcd set.
     """
@@ -46,9 +46,6 @@ def sched_update(sched=None):
     if isinstance(sched, list):
         print('concat sched list')
         sched = concat(sched)
-    elif sched is None:
-        fn = ls.get_dict('/cmd/observing/sdfest')
-        sched = concat(sched, make_sched(fn))
         
     sched.sort_index(inplace=True)
     sched = sched[sched.index > Time.now().mjd]
@@ -57,18 +54,19 @@ def sched_update(sched=None):
     return sched
 
 
-def submit_next(mjd, row, pool):
+def submit_next(sched, pool):
     """ Waits for mjd and submits the schedule row to the pool
     """
+    # alternatively, make sessions into a sequence with one start time
 
-    if Time.now().mjd < mjd:  # alternatively, make sessions into a sequence with one start time
-        sleepmjd = mjd - Time.now().mjd
-        print(f"Waiting {sleepmjd*24*3600} seconds to run command ({row.command})...")
-        sleep(sleepmjd*24*3600)
-
-    fut = pool.submit(runrow, row)
-
-    return fut
+    row = sched.iloc[0]
+    mjd = row.name
+    if Time.now().mjd - mjd < 1/(24*3600):
+        fut = pool.submit(runrow, row)
+        return fut
+#        sleepmjd = mjd - Time.now().mjd
+#        print(f"Waiting {sleepmjd*24*3600} seconds to run command ({row.command})...")
+#        sleep(sleepmjd*24*3600)
 
 
 def runrow(row):
@@ -81,26 +79,34 @@ def runrow(row):
 
 
 ls = dsa_store.DsaStore()
-ls.add_watch('/cmd/observing/sdfest', sched_update)
+sched0 = DataFrame([])
+def sched_callback():
+    def a(event):
+        global sched0
+        sched = make_sched(event)
+        sched0 = sched_update([sched0, sched])
+    return a
+ls.add_watch('/cmd/observing/sdfname', sched_callback())
 
 
 if __name__ == "__main__":
     """ Run commands parsed from SDF.
     """
 
-    # TODO: initialize by setting up etcd callbacks
-    sched = make_sched(sys.argv[1])
-    for fn in sys.argv[2:]:
-        sched2 = make_sched(fn)
-        sched = sched_update([sched, sched2])
+    if len(sys.argv) > 2:
+        sched0 = make_sched(sys.argv[1])
 
     futures = []
+    nextmjd = 0
     with ProcessPoolExecutor(max_workers=8) as pool:
         while True:
             try:
-                for (mjd, row) in sched.iterrows():   # should always have future dated rows
-                    fut = submit_next(mjd, row, pool)    # wait, then fire and forget
+                if len(sched0):
+                    fut = submit_next(sched0, pool)    # fire and forget
                     futures.append(fut)
+                    if sched0.iloc[0].name > nextmjd:
+                        nextmjd = sched0.iloc[0].name
+                        print(f"Next submission at MJD {nextmjd}, in {(nextmjd-Time.now().mjd)*24*3600}s")
             except KeyboardInterrupt:
                 print("Interrupting execution of schedule. Waiting on submissions (Ctrl-C again to interrupt)...")
                 try:
@@ -116,5 +122,4 @@ if __name__ == "__main__":
 
             # clean up futures
             futures = [fut for fut in futures if fut.done() or fut.cancelled()]
-            print(f"Processing {len(futures)} submissions.")
-            sleep(1)
+            sleep(0.49)  # at least two per second
