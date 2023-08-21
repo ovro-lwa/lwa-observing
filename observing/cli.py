cd6774af3a3bcc5cf275c4e6345a73cf00e6dc99
Casey Law <caseyjlaw@gmail.com>
2023-08-21 09:43:53 -0700

    adding some notes for new tools

--- observing/cli.py
+++ observing/cli.py
@@ -8,6 +8,16 @@ ls = dsa_store.DsaStore()
 def cli():
     pass
 
+# lwaobserving command-line tool ideas:
+# - start slow vis now
+# - make sdf for asap bf or fastvis obs
+# - run calibration pipeline?
+# Other tool "lwamnc"?
+# - print go/no-go status
+# - change settings (absorb other scripts)
+# - turn off arx power
+# - ascii plot of f-eng auto spectra
+
 @cli.command()
 @click.argument('sdffile')
 #@click.option('--asap', is_flag=True, default=False, show_default=True)
@@ -17,3 +27,9 @@ def submit_sdf(sdffile):
 
     assert os.path.exists(sdffile), f"file {sdffile} not found"
     ls.put_dict('/cmd/observing/sdfname', sdffile)
+
+
+#def start_dr(recorder='', config='', duration=None):
+#    from mnc import control
+#    con = control.Controller(config=config, recorder=recorder)
+#    con.start_dr(recorder, duration=duration)
