ab873d9c80ce23f86e6e07f118b8c07bb48baea7
Casey Law <caseyjlaw@gmail.com>
2023-12-04 23:16:28 +0000

    Update session status in sched_update function

--- scripts/executor.py
+++ scripts/executor.py
@@ -41,9 +41,9 @@ def sched_update(sched, mode='buffer'):
                     else:
                         logger.warning(f"Removing session starting at {s0.index[0]}")
                         try:
-                            obsstate.update_session(s0['session_id'], 'skipped')
+                            obsstate.update_session(str(s0['session_id']), 'skipped')
                         except Exception as exc:
-                            logger.warning(f"Could not update session status: {exc}.")
+                            logger.warning(f"Could not update session status: {str(exc)}.")
 
             sched = concat(include)
         
