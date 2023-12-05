fcf84ff9d2cde91d97b745cc87361bbe8ca3f5d9
Casey Law <caseyjlaw@gmail.com>
2023-12-05 01:36:51 +0000

    Fix variable name in reset_schedule and cancel_sdf functions

--- observing/cli.py
+++ observing/cli.py
@@ -68,7 +68,7 @@ def reset_schedule(hard):
     hard reset will cancel observation currently being observed (experimental).
     """
 
-    ls.put_dict('/cmd/observing/submitsdf', {'sdffile': None, 'mode': 'reset'})
+    ls.put_dict('/cmd/observing/submitsdf', {'filename': None, 'mode': 'reset'})
     if hard:
         raise NotImplementedError
 
@@ -79,7 +79,7 @@ def cancel_sdf(sdffile):
     """ Use SDF to remove session from schedule
     """
 
-    ls.put_dict('/cmd/observing/submitsdf', {'sdffile': sdffile, 'mode': 'cancel'})
+    ls.put_dict('/cmd/observing/submitsdf', {'filename': sdffile, 'mode': 'cancel'})
 
 
 @cli.command()
