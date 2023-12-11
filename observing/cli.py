b5c7dbff76ce70598ca2845a84df18b35eda6cda
Casey Law <caseyjlaw@gmail.com>
2023-12-11 22:18:38 +0000

    Update CLI options in observing/cli.py

--- observing/cli.py
+++ observing/cli.py
@@ -43,16 +43,16 @@ def submit_sdf(sdffile, asap, reset):
 
 @cli.command()
 @click.argument('sdffile')
-@click.option('--n_obs', default=1, type=int, help='Number of observations to create')
-@click.option('--sess_mode', default='POWER', type=str, help='Session mode (FAST, SLOW, POWER, VOLT)')
-@click.option('--beam_num', default=None, type=int, help='POWER/VOLT beam number')
-@click.option('--obs_mode', default='TRK_RADEC', type=str, help='Observation mode (e.g. TRK_RADEC, TRK_JUPITER, TRK_SOLAR, TRK_LUNAR)')
-@click.option('--obs_start', default=None, type=str, help='Observation start time (UTC) in YYYY-MM-DDTHH:MM:SS format or "now"')
-@click.option('--obs_dur', default=None, type=int, help='Observation duration in milliseconds')
+@click.option('--n-obs', default=1, type=int, help='Number of observations to create')
+@click.option('--sess-mode', default='POWER', type=str, help='Session mode (FAST, SLOW, POWER, VOLT)')
+@click.option('--beam-num', default=None, type=int, help='POWER/VOLT beam number')
+@click.option('--obs-mode', default='TRK_RADEC', type=str, help='Observation mode (e.g. TRK_RADEC, TRK_JUPITER, TRK_SOLAR, TRK_LUNAR)')
+@click.option('--obs-start', default=None, type=str, help='Observation start time (UTC) in YYYY-MM-DDTHH:MM:SS format or "now"')
+@click.option('--obs-dur', default=None, type=int, help='Observation duration in milliseconds')
 @click.option('--ra', default=None, type=float, help='RA of object to track (in hours)')
 @click.option('--dec', default=None, type=float, help='Dec of object to track (in degrees)')
-@click.option('--obj_name', default=None, type=str, help='Name of object to track (used as alternative to RA/Dec)')
-@click.option('--int_time', default=None, type=int, help='Integration time in milliseconds')
+@click.option('--obj-name', default=None, type=str, help='Name of object to track (used as alternative to RA/Dec)')
+@click.option('--int-time', default=None, type=int, help='Integration time in milliseconds')
 def create_sdf(sdffile, n_obs, sess_mode, beam_num, obs_mode, obs_start, obs_dur, ra, dec, obj_name, int_time):
     """ Create an SDF file.
     """
