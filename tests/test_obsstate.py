40ede371697f85d513625234750e72522ea36a13
Casey Law <caseyjlaw@gmail.com>
2024-03-01 22:28:26 +0000

    Fix create_db function and add test for add_calibrations

--- tests/test_obsstate.py
+++ tests/test_obsstate.py
@@ -1,17 +1,12 @@
 import pytest
 from observing.obsstate import create_db, connection_factory, add_calibrations
 
-def test_add_calibrations():
-    # Test that the calibrations are added successfully
-    create_db('./ovrolwa_test.db')
-
-    add_calibrations('test', 1)
-
 
 def test_create_db():
     # Test that the tables are created successfully
-    create_db()
+    create_db('./ovrolwa_test.db')
 
+    add_calibrations('test', 1)
     # Check if the tables exist in the database
     with connection_factory('./ovrolwa_test.db') as conn:
         c = conn.cursor()
