c96e9207d55b754e57c4e7d079c46c96ff827782
Casey Law <caseyjlaw@gmail.com>
2024-03-01 22:31:21 +0000

    fix

--- tests/test_obsstate.py
+++ tests/test_obsstate.py
@@ -6,7 +6,12 @@ def test_create_db():
     # Test that the tables are created successfully
     create_db('./ovrolwa_test.db')
 
-    add_calibrations('test', 1)
+    now = Time.now().mjd
+    with connection_factory('./ovrolwa_test.db') as conn:
+        c = conn.cursor()
+        c.execute("INSERT INTO calibrations (time_loaded, filename, beam) VALUES (?, ?, ?)", (float(now), str(filename), str(beam)))
+
+
     # Check if the tables exist in the database
     with connection_factory('./ovrolwa_test.db') as conn:
         c = conn.cursor()
