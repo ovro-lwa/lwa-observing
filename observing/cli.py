aab645ab77e96d84182065ec4ab18de446b17413
Casey Law <caseyjlaw@gmail.com>
2024-08-05 23:09:32 +0000

    typo _

--- observing/cli.py
+++ observing/cli.py
@@ -42,7 +42,7 @@ def submit_sdf(sdffile, asap, reset):
         session_mode_name = f"{dd['SESSION']['SESSION_ID']}_{dd['SESSION']['SESSION_MODE']}"
         if 'SESSION_DRX_BEAM' in dd['SESSION']:
             session_mode_name += dd['SESSION']['SESSION_DRX_BEAM']
-        if session_mode name in ls.get_dict('/mon/observing/sdfdict'):
+        if session_mode_name in ls.get_dict('/mon/observing/sdfdict'):
             print(f"Warning: SDF {sdffile} was parsed by scheduler before. Was this SDF already submitted?")
 
         sched = parsesdf.make_sched(sdffile)
